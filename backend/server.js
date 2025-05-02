bcd571ab14469953bfd7a6dadbff2180d4462723
Tasheen2002 <darshikatasheen99@gmail.com>
2025-05-02 11:02:19 +0530

    updates

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+import express from "express"; //framework of node.js
+import dotenv from "dotenv";
+import { connectDB } from "./config/db";
+
+dotenv.config();
+
+const app = express(); //express is a function. when calling it it returns a object that store in a variable called app.
+//app.listen is the one of the function in the app object.
+
+console.log(process.env.MONGO_URI);
+
+app.get("/", (req, res) => {
+  //when someone open the URL the server responds with the text.
+  res.send("server is ready");
+});
+
+app.listen(5000, () => {
+  connectDB();
+  console.log("Server started at http://localhost:5000");
+});
