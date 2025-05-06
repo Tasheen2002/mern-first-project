22ce00e9ee48c6124da6bc17e5a56f001e23b030
Tasheen2002 <darshikatasheen99@gmail.com>
2025-05-06 12:41:18 +0530

    connected server to the database

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,13 @@
 import express from "express"; //framework of node.js
 import dotenv from "dotenv";
-import { connectDB } from "./config/db";
+import { connectDB } from "./config/db.js";
 
 dotenv.config();
 
 const app = express(); //express is a function. when calling it it returns a object that store in a variable called app.
 //app.listen is the one of the function in the app object.
 
-console.log(process.env.MONGO_URI);
+// console.log(process.env.MONGO_URI);
 
 app.get("/", (req, res) => {
   //when someone open the URL the server responds with the text.
