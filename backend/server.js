e21cee543c0de71e0b082ce4a46f1a5c7d4021d3
Tasheen2002 <darshikatasheen99@gmail.com>
2025-05-07 10:20:05 +0530

    crreate product functionality created

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express"; //framework of node.js
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import Product from "./models/product.model.js";
 
 dotenv.config();
 
@@ -9,10 +10,28 @@ const app = express(); //express is a function. when calling it it returns a obj
 
 // console.log(process.env.MONGO_URI);
 
-app.get("/", (req, res) => {
-  //when someone open the URL the server responds with the text.
-  res.send("server is ready");
+app.use(express.json()); //allow us to accept json data in the req.body
+
+app.post("/api/products", async (req, res) => {
+  const product = req.body;
+
+  if (!product.name || !product.price || !product.image) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Please provide all fields" });
+  }
+
+  const newProduct = new Product(product);
+  try {
+    await newProduct.save();
+    res.status(201).json({ success: true, data: newProduct });
+  } catch (error) {
+    console.error("Error in create product:", error.message);
+    res.status(500).json({ success: false, message: error.message });
+  }
 });
+
+
 
 app.listen(5000, () => {
   connectDB();
